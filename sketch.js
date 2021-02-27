09cba34d214a0eef9c359c79530a90d23c5b2c28
Ayman12345678910 <74134763+Ayman12345678910@users.noreply.github.com>
2021-02-27 14:11:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,13 +30,56 @@ block9= new Block(390,155,30,40)
 function draw() {
     background("black");
     Engine.update(engine);
-    block1.display();
-    block2.display();
-    block3.display();
-    block4.display();
-    block5.display();
-    block6.display();
-    block7.display();
-    block8.display();
-    block9.display();
+ textSize(20);
+  fill("lightyellow");
+  text("Drag the Hexagonal Stone and Release it, to launch it towards the blocks",100,30);
+
+  ground.display();
+  stand1.display();
+  stand2.display();
+  strokeWeight(2);
+  stroke(15);
+  fill("skyblue");
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  fill("pink");
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  fill("turquoise");
+  block13.display();
+  block14.display();
+  block15.display();
+  fill("grey");
+  block16.display();
+  fill("skyblue");
+  blocks1.display();
+  blocks2.display();
+  blocks3.display();
+  blocks4.display();
+  blocks5.display();
+  fill("turquoise");
+  blocks6.display();
+  blocks7.display();
+  blocks8.display();
+  fill("pink")
+  blocks9.display();
+  fill("gold");
+  imageMode(CENTER)
+  image(polygon_img ,ball.position.x,ball.position.y,40,40);
+
+  slingShot.display();
+}
+function mouseDragged(){
+  Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
+}
+function mouseReleased(){
+  slingShot.fly();
 }
